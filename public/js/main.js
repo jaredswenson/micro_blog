8207e366720cf4ea50868e32f6270d29829885ee
Brad Flint <bradwflint@gmail.com>
2016-12-06 11:29:22 -0700

    added images, etc

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-main.js
+$(document).ready(function() {
+    $("#b").animate({left: "+=500"}, 2000);
+    $("#b").animate({left: "-=300"}, 1000);
+});
+
+function birdLeft() {
+    $("#b").animate({left: "-=500"}, 2000, "swing", birdRight);
+}
+function birdRight() {
+    $("#b").animate({left: "+=500"}, 2000, "swing", birdLeft);
+}
+
+birdRight();
